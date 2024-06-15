513d370620f9f827e0a2d02c4166c1a5ad8257ad
7minutech <elijohnson1107@gmail.com>
2024-06-15 15:46:30 -0500

    Updated logic and added comments

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const container = document.querySelector("#container")
-const rows = document.querySelectorAll(".rows");
  
-//element.setAttribute("style","width: 200px; height: 200px;")
 function createGrid(row,col){
     for(let i = 0;i<row;i++){
+        //each row gets class row
         let divRow = document.createElement("div");
         divRow.setAttribute("class",`row`);
-        divRow.setAttribute("style","display: flex; flex-wrap: wrap;flex-grow: 1;")
         container.appendChild(divRow);
-        
+        //each column gets class tile
         for(let j = 0;j<col;j++){
             let tile = (document.createElement("div"));
             divRow.appendChild(tile);
@@ -18,9 +16,15 @@ function createGrid(row,col){
     }
 }
 
-createGrid(16,16);
+createGrid(16,16)
+//set style to rows
+const rows = document.querySelectorAll(".row");
+rows.forEach((row) => {
+    row.setAttribute("style","display: flex; flex-wrap: wrap;flex-grow: 1;")
+})
+//sets style to tiles
 const elements = document.querySelectorAll(".tile");
 elements.forEach((element) => {
-    element.setAttribute("style","border: 1px solid black; flex: 1; background-color; red")
+    element.setAttribute("style","border: 1px solid red; flex: 1; background-color; red")
 
 })
